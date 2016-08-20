056342361e202ae20c81e287545227c9fc2271e2
phill765 <league@WTS13.attlocal.net>
2016-08-19 18:13:27 -0700

    constructors

---
+++ src/HarryPotter.java
@@ -0,0 +1,50 @@
+
+
+	 
+
+	public class HarryPotter {
+
+		private boolean cloakOn;
+		
+		HarryPotter() {
+			System.out.println("making Harry Potter...");
+		}
+
+		void castSpell(String spell){
+	System.out.println("casting spell: " + spell);
+		}
+
+		void makeInvisible(boolean invisible) {
+			this.cloakOn = invisible;
+			
+			if (cloakOn)
+				System.out.println("Harry is invisible");
+			else
+				System.out.println("Harry is visible");
+		}
+
+		void spyOnSnape() {
+			System.out.println("Harry sees Professor Snape doing nefarious things.");
+		}
+
+		public static void main(String[] args) {
+			// 1. make harry potter
+			HarryPotter kid = new HarryPotter();
+			// 2. become invisible
+			kid.makeInvisible(true);
+			// 3. spy on professor snape
+			kid.spyOnSnape();
+			// 4. become visible again
+			kid.makeInvisible(false);
+			// 5. cast a “stupefy” spell
+			kid.castSpell("Stupefy");
+		}
+
+	}
+
+
+
+
+
+
+
--- src/guy.java
+++ src/guy.java
@@ -5,15 +5,18 @@ public class guy {
 	
 	guy(String name){
 		this.name=name;
+		int money=10; 
+	
 	}
 	public void walletSwype(){
 		System.out.println("Credit card number:4444444444444444");
-		System.out.println("Social Security Number:1010001");
+		System.out.println("Social Security Number:1000101");
 		
 	}
 	public void mug(){
 		System.out.println("Please don't hurt me I only " + money + " $");
 	}
+	//public void
 	public static void main(String[] args) {
 		guy guy= new guy("bill");
 		guy.mug();
